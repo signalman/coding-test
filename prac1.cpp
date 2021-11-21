363efa565b43a957e479262b962cca96a8a1d3cd
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-11-21 22:35:03 +0900

    c++: for

--- prac1.cpp
+++ prac1.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  int *a;
-  int b= 10;
-  a=&b;
-  cout << *a <<endl;
-  cout << a <<endl;
-  
+int main()
+{
+  int n;
+  cin>>n;
+
+  for(int i =1;i<=n;i++){
+    int a=i%10;
+    if(a==3||a==6||a==9){
+      cout<< "* ";
+    }
+    else {
+      cout<< i<< ' ';
+    }
+  }
 
   return 0;
 }
+// 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
