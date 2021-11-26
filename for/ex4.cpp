9e8a5aca98a032f702f7b4f0bdad7403eefc5720
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-11-26 23:21:23 +0900

    cpp: for~

---
+++ for/ex4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cin.tie(NULL);
+  int T;
+
+  cin>>T;
+  for(int i=0;i<T;i++){
+    int a, b;
+    cin>>a>>b;
+    cout<<a+b<<'\n';
+  }
+}
---
+++ function/ex5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int *a= new int(5);
+
+  cout<< a <<endl;
+  cout << a+1 << endl;
+  cout<< *a <<endl;
+}
