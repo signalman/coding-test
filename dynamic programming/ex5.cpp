79be7df4c9aece64c37dd2353cbb1e510aee7dbf
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-12-08 22:56:22 +0900

    dynamic programming

---
+++ dynamic programming/ex5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int p[1005];
+int dp[1005];
+
+int main(){
+  int n;
+  cin>>n;
+  for(int i=1; i<=n; i++){
+    cin>>p[i];
+  }
+  dp[0]=0;
+  dp[1]=p[1];
+  for(int i=1; i<=n; i++){
+    dp[i]=10000;
+    for(int j=1; j<=i; j++){
+      dp[i]=min(dp[i],dp[i-j]+p[j]);
+    }
+  }
+
+  cout<<dp[n]<<'\n';
+
+  return 0;
+}
