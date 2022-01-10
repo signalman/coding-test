4746b5eb880b9eb022e21e519a5cd7e33dbb66db
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2022-01-10 19:22:31 +0900

    c++: back tracking

---
+++ 재귀/ex4.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int m;
+int ans;
+
+int fi(int n){
+  if(n==0){return 0;}
+  if(n==1){return 1;}
+  return fi(n-1)+fi(n-2);
+}
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin>>m;
+  cout<<fi(m);
+}
