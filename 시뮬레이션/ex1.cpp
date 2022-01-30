77425476f2924e3d46ad2830640cdadbb71d613b
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2022-01-30 12:40:26 +0900

    c++:시뮬레이션

--- 시뮬레이션/ex1.cpp
+++ 시뮬레이션/ex1.cpp
@@ -1,23 +1,21 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
+
 #define X first
 #define Y second
-
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
 int board1[10][10];
 int board2[10][10];
-//남동북서
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int n,m;
 vector<pair<int, int>> cctv;
+int n, m;
 
-bool OOB(int a, int b){
-  return a < 0 || a >= n || b < 0 || b >= m;
+bool OOB(int x, int y){
+  return (x < 0 || y < 0 || x >= n || y >= m);
 }
-
 void upd(int x, int y, int dir){
   dir %= 4;
-  while(true){
+  while(1){
     x += dx[dir];
     y += dy[dir];
     if(OOB(x, y) || board2[x][y] == 6) return;
@@ -34,12 +32,12 @@ int main(){
   for(int i=0; i<n; i++){
     for(int j=0; j<m; j++){
       cin>>board1[i][j];
-      if(board1[i][j] != 0 && board1[i][j] != 6){cctv.push_back({i, j});}
       if(board1[i][j] == 0) mn++;
+      if(board1[i][j] != 0 && board1[i][j] != 6) cctv.push_back({i, j});
     }
   }
-  
-  for(int tmp = 0 ; tmp < (1<<(2*cctv.size())); tmp++){
+
+  for(int tmp = 0; tmp < 1<<(2*cctv.size()); tmp++){
     for(int i=0; i<n; i++){
       for(int j=0; j<m; j++){
         board2[i][j] = board1[i][j];
@@ -51,16 +49,33 @@ int main(){
       brute /= 4;
       int x = cctv[i].X;
       int y = cctv[i].Y;
-      if(board1[x][y] == 1){upd(x, y, dir);}
-      else if(board1[x][y] == 2){upd(x, y, dir); upd(x, y, dir+2);}
-      else if(board1[x][y] == 3){upd(x, y, dir); upd(x, y, dir+1);}
-      else if(board1[x][y] == 4){upd(x, y, dir); upd(x, y, dir+1); upd(x, y, dir+2);}
-      else{upd(x, y, dir); upd(x, y, dir+1); upd(x, y, dir+2); upd(x, y, dir+3);}
+      if(board2[x][y] == 1){
+        upd(x, y, dir);
+      }
+      else if(board2[x][y] == 2){
+        upd(x, y, dir);
+        upd(x, y, dir+2);
+      }
+      else if(board2[x][y] == 3){
+        upd(x, y, dir);
+        upd(x, y, dir+1);
+      }
+      else if(board2[x][y] == 4){
+        upd(x, y, dir);
+        upd(x, y, dir+1);
+        upd(x, y, dir+2);
+      }
+      else{
+        upd(x, y, dir);
+        upd(x, y, dir+1);
+        upd(x, y, dir+2);
+        upd(x, y, dir+3);
+      }
     }
     int val = 0;
     for(int i=0; i<n; i++){
       for(int j=0; j<m; j++){
-        val += (board2[i][j] == 0);
+        if(board2[i][j] == 0) val++;
       }
     }
     mn = min(mn, val);
