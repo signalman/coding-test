940c2ecb5836c730388a3e8d3f793418e48f9d68
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2022-02-20 17:32:56 +0900

    c++: 큐빙

--- SW역량테스트/tempCodeRunnerFile.cpp
+++ SW역량테스트/tempCodeRunnerFile.cpp
@@ -1,3 +1,13 @@
-  // for(int i=0; i<m.size(); i++){
-  //   cout<<m[i];
-  // }
+case 'L':
+        if(s[1]=='+'){
+          swap(cube[0][0][0],cube[2][0][0]);
+          swap(cube[0][1][0],cube[2][1][0]);
+          swap(cube[0][2][0],cube[2][2][0]);
+          
+          swap(cube[0][0][0],cube[3][0][2]);
+          swap(cube[0][1][0],cube[3][1][2]);
+          swap(cube[0][2][0],cube[3][2][2]);
+
+          swap(cube[1][0][0],cube[3][0][2]);
+          swap(cube[1][1][0],cube[3][1][2]);
+          swap(cube[1][2][0],cube[3][2][2]);
