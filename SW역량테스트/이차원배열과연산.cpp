3c2f38bcaa79d7d9a1e68cc48f121a05c6581bcd
apicad0307 <apicad0307@gmail.com>
2022-03-11 01:34:54 +0900

    이차원배열과연산

--- SW역량테스트/이차원배열과연산.cpp
+++ SW역량테스트/이차원배열과연산.cpp
@@ -1,10 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define X first
+#define Y second
 
 int arr[102][102];
 int n = 3;
 int m = 3;
 int r, c, k;
+
+void transArr(){
+  int mx = max(n, m);
+  for(int i=1; i<=mx; i++){
+    for(int j=i+1; j<=mx; j++){
+      swap(arr[i][j], arr[j][i]);
+    }
+  }
+  swap(n, m);
+}
+
+void sortArr(int arr[]){
+  int freqArr[102] = {};
+  for(int i=1; i<=n; i++){
+    if(!arr[i])continue;
+    freqArr[arr[i]]++;
+  }
+  vector<pair<int, int>> v;
+  for(int i=1; i<=100; i++){
+    if(!freqArr[i])continue;
+    v.push_back({freqArr[i], i});
+  }
+  sort(v.begin(), v.end());
+  int j=0;
+  for(auto c: v){
+    arr[++j] = c.Y;
+    arr[++j] = c.X;
+  }
+  n = max(n, j);
+  fill(arr+j+1, arr+n+1, 0);
+}
+
 
 int main(){
   ios_base::sync_with_stdio(0);
@@ -18,11 +52,13 @@ int main(){
   int t = 0; 
   while( t<=100 && arr[r][c] != k){
     bool transposed = false;
-    if(n>m){
+    if(m<n){
       transArr();
       transposed = true;
     }
-    sortArr();
+    for(int i=1; i<=m; i++){
+      sortArr(arr[i]);
+    }
     if(transposed){
       transArr();
     }
