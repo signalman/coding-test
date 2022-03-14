e30e15e25deff17a82d542d656e45b46438d7774
apicad0307 <apicad0307@gmail.com>
2022-03-14 18:12:29 +0900

    프로세서 연결하기

---
+++ SW역량테스트/새로운게임2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int color[13][13];
+int dx[] = {0, 0, 0, -1, 1};
+int dy[] = {0, 1, -1, 0, 0};
+struct pieces{
+  int cnt;
+  int dir;
+  int ord[11];
+};
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+}
