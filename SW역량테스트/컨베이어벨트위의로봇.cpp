a0268eb0130b20f185e1b1a1f50cbc55fc617706
apicad0307 <apicad0307@gmail.com>
2022-03-31 23:01:14 +0900

    update

--- SW역량테스트/컨베이어벨트위의로봇.cpp
+++ SW역량테스트/컨베이어벨트위의로봇.cpp
@@ -1,56 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define X first
+#define Y second
 
-vector<int> belt;
-vector<int> A;
-vector<int> robot;
+
+bool belt[101]; //벨트위에 로봇이 있는지 저장
+int A[101]; //벨트의 내구성 저장
 int n, k;
-int up, down;
 int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cin>>n>>k;
   for(int i=0; i<2*n; i++){
-    int a;
-    cin>>a;
-    A.push_back(a);
+    cin>>A[i];
   }
-  belt.reserve(2*n);
   int cnt = 0;
-  up = 0;
-  down = n;
+  int up = 0;
+  int down = n-1;
   while(1){
-    
-
-
-    vector<int> temp(2*n);
     cnt++;
-    for(int i=0; i<2*n; i++){
-      belt[i] = temp[(i+1)%(2*n)];
+    //1번
+    if(belt[down]){
+      belt[down] = 0;
     }
-    belt = temp;
-    up= ((up-1)+2*n)%(2*n) , down = ((down-1)+2*n)%(2*n);
-    for(int i=0; i<robot.size(); i++){
-      if(belt[(robot[i]+1)%(2*n)] == 0 && A[(robot[i]+1)%(2*n)] != 0) {
-        robot[i] = (robot[i] + 1) % (2*n);
-        A[(robot[i]+1)%(2*n)]--;
+    down = (down - 1 + 2*n)%(2*n);
+    up = (up - 1 + 2*n)%(2*n);
+    int move = down;
+    while(move!=up){
+      if(move == down){belt[move] = 0; continue;}
+      if(belt[move] == 1 && belt[(move + 1 + 2*n)%(2*n)] == 0 && A[(move + 1 + 2*n)%(2*n)]>=1){
+        belt[move] = 0;
+        belt[(move + 1 + 2*n)%(2*n)] = 1;
+        A[(move + 1 + 2*n)%(2*n)]--;
       }
+      move = (move-1+2*n)%(2*n);
     }
-
     if(A[up] != 0){
-      robot.push_back(up);
       belt[up] = 1;
     }
-    
     int t = 0;
     for(int i=0; i<2*n; i++){
-      if(A[i]==0){
+      if(!A[i]){
         t++;
       }
     }
-    if(t>=k) break;
+    if(t>=k){
+      break;
+    }
   }
-
   cout<<cnt;
-  return 0;
 }
