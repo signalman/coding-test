5ba9a845d4bbdd40c9df91b39136ef0cf16349d7
apicad0307 <apicad0307@gmail.com>
2022-04-16 22:11:35 +0900

    Update 마법사상어와 토네이도.cpp

--- SW역량테스트/마법사상어와 토네이도.cpp
+++ SW역량테스트/마법사상어와 토네이도.cpp
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//왼쪽, 아래, 오른쪽, 위
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+
+int n;
+int MAP[501][501];
+
+//함수(방향, 반복)
+void sol(int x, int y, int dir, int dist){
+  if(dir == 0){
+
+  }
+  else if(dir == 1){
+
+  }
+  else if(dir == 2){
+
+  }
+  else{
+
+  }
+
+  
+}
+
+
+
+
 int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(0);
