40ea985b72d59024a2a6f1b1a4667633e4ae5a7c
apicad0307 <apicad0307@gmail.com>
2022-04-17 19:04:19 +0900

    Update 마법사상어와 토네이도.cpp

--- SW역량테스트/마법사상어와 토네이도.cpp
+++ SW역량테스트/마법사상어와 토네이도.cpp
@@ -1,83 +1,56 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+//왼쪽, 아래, 오른쪽, 위
+int dx[] = {0, 1, 0, -1};
+int dy[] = {-1, 0, 1, 0};
 
-int N;
-int A[499][499];
-const int dy[]={0,1,0,-1};
-const int dx[]={-1,0,1,0};
-const int ratio[9]={1,1,2,7,7,2,10,10,5};
-const int blowY[4][10]={
-    {-1,1,-2,-1,1,2,-1,1,0,0},
-    {-1,-1,0,0,0,0,1,1,2,1},
-    {-1,1,-2,-1,1,2,-1,1,0,0},
-    {1,1,0,0,0,0,-1,-1,-2,-1}
+int n;
+int MAP[500][500];
+int sandX[4][10] = {
+  {0, -1, 1, -2, -1, 1, 2, -1, 1, 0},
+  {2, 1, 1, 0, 0, 0, 0, -1, -1, 1},
+  {0, -1, 1, -2, -1, 1, 2, -1, 1, 0},
+  {-2, -1, -1, 0, 0, 0, 0, 1, 1, -1}
 };
-const int blowX[4][10]={
-    {1,1,0,0,0,0,-1,-1,-2,-1},
-    {-1,1,-2,-1,1,2,-1,1,0,0},
-    {-1,-1,0,0,0,0,1,1,2,1},
-    {-1,1,-2,-1,1,2,-1,1,0,0}
+int sandY[4][10] = {
+  {-2, -1, -1, 0, 0, 0, 0, 1, 1, -1},
+  {0, -1, 1, -2, -1, 1, 2, -1, 1, 0},
+  {2, 1, 1, 0, 0, 0, 0, -1, -1, 1},
+  {0, -1, 1, -2, -1, 1, 2, -1, 1, 0}
 };
-
-bool inRange(int y, int x){
-    return y>=0 && x>=0 && y<N && x<N;
+int rate[9] = {5, 10, 10, 2, 7, 7, 2, 1, 1};
+int ans;
+bool OOB(int x, int y){
+  return !(x<0||y<0||x>=n||y>=n);
 }
 
-// 모래의 흩날림 처리와 격자 밖 모래 반환
-int blowSand(int y, int x, int dir){
-    int ret=0, init=A[y][x];
-
-    for(int i=0;i<10;i++){
-        int sand;
-        if(i!=9){
-            sand=init*ratio[i]/100;
-            A[y][x]-=sand;
-        }
-        else sand=A[y][x];
-
-        int by=y+blowY[dir%4][i];
-        int bx=x+blowX[dir%4][i];
-
-        if(!inRange(by,bx)){
-            ret+=sand;
-            continue;
-        }
-
-        A[by][bx]+=sand;
+int wind(int dir, int x, int y){
+  int ret = 0;
+  int sand = MAP[x][y];
+  for(int i=0; i<10; i++){
+    int nx = x + sandX[dir][i];
+    int ny = y + sandY[dir][i];
+    if(i != 9){
+      MAP[nx][ny] += sand * rate[i] / 100;
+      MAP[x][y] -= sand * rate[i] / 100;
     }
-
-    A[y][x]=0;
-
-    return ret;
-}
-
-int solve(){
-    int ret=0, y=N/2, x=N/2, dir=0;
-
-    // 토네이도 이동
-    for(double i=1.0;i<=N;i+=0.5){
-        for(int j=0;j<(int)i;j++){
-            y+=dy[dir%4];
-            x+=dx[dir%4];
-
-            ret+=blowSand(y,x,dir);
-        }
-        dir++;
+    else{
+      MAP[x + sandX[dir][i]][y + sandY[dir][i]] = MAP[x][y];
+      MAP[x][y] = 0;
     }
-
-    return ret;
+  }
+  return ret;
 }
 
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin>>n;
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      cin>>MAP[i][j];
+    }
+  }
+  
 
-    cin>>N;
-    for(int i=0;i<N;i++)
-        for(int j=0;j<N;j++)
-            cin>>A[i][j];
-
-    cout<<solve();
-
-    return 0;
 }
