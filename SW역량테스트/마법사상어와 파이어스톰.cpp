bfc257c6ca6965f620dfd4e2c22dd0a3865d0498
apicad0307 <apicad0307@gmail.com>
2022-04-25 21:06:16 +0900

    Create 마법사상어와 파이어스톰.cpp

---
+++ SW역량테스트/마법사상어와 파이어스톰.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//돌리고 인접면이 2개 이하인 칸에대해 1이 줄어든다
+int n, q;
+int MAP[65][65];
+queue<int> level;
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin>>n>>q;
+  for(int i=0; i<(1<<n); i++){
+    for(int j=0; j<(1<<n); j++){
+      cin>>MAP[i][j];
+    }
+  }
+  for(int i=0; i<q; i++){
+    int t;
+    cin>>t;
+    level.push(t);
+  }
+
+  while(!level.empty()){
+    int cur = level.front();
+    level.pop();
+    
+  }
+}
