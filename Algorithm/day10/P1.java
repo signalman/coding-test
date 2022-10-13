8c03bb444c117468d220d186bf29c1e19c2c4c30
hoin <apicad0307@gmail.com>
2022-10-13 19:13:35 +0900

    algorithm

--- Algorithm/day10/P1.java
+++ Algorithm/day10/P1.java
@@ -3,102 +3,93 @@ package Algorithm.day10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class P1 {
-    static int moveY[] = {-1,0,1,0,0,0};
-    static int moveX[] = {0,1,0,-1,0,0};
-    static int moveZ[] = {0,0,0,0,1,-1};
-    static int L,R,C;
-    static char arr[][][];
-    static int map[][][];
-    static boolean visit[][][],check=false;
-    static Po start, end;
+    static int[] dz = {0, 0, 0, 0, 1, -1}; //높이
+    static int[] dx = {0, 1, -1, 0, 0, 0}; //행
+    static int[] dy = {1, 0, 0, -1, 0, 0}; //열
+    static char[][][] MAP = new char[31][31][31];
+    static int L, R, C;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static ArrayList<String> ret = new ArrayList<>();
+    static int tx, ty, tz;
+    static class Tuple{
+        int z, x, y;
+        int dist;
+        Tuple(int z, int x, int y){ //높이, 행, 열
+            this.z = z;
+            this.x = x;
+            this.y = y;
+        }
+    }
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while(true) {
-            check = false;
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            if(!st.hasMoreTokens())
-                st = new StringTokenizer(br.readLine());
 
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             L = Integer.parseInt(st.nextToken());
             R = Integer.parseInt(st.nextToken());
             C = Integer.parseInt(st.nextToken());
-            if(L==0 && R==0 && C==0) {
-                break;
-            }
-            arr = new char[L][R][C];
-            map = new int[L][R][C];
-            visit = new boolean[L][R][C];
+            if(L==0&&R==0&&C==0) break;
 
-            for(int l=0; l<L; l++) {
-                for(int r=0; r<R; r++) {
+            Queue<Tuple> queue = new LinkedList<>();
+            int dist[][][] = new int[31][31][31];
+            for(int i=0; i<L; i++){
+                for(int j=0; j<R; j++){
                     String str = br.readLine();
-
-                    if(str.equals(""))
-                        str = br.readLine();
-
-                    for(int c=0; c<C; c++) {
-                        char ch = str.charAt(c);
-                        if(ch == 'S') {
-                            start = new Po(c,r,l);
-                        }else if(ch == 'E') {
-                            end = new Po(c,r,l);
+                    for(int k=0; k<C; k++){
+                        char ch = str.charAt(k);
+                        MAP[i][j][k] = ch;
+                        if(ch == 'S'){
+                            queue.offer(new Tuple(i, j, k));
+                            dist[i][j][k] = 1;
                         }
-                        arr[l][r][c] = ch;
+                        if(ch == 'E'){
+                            tz = i;
+                            tx = j;
+                            ty = k;
+                        }
                     }
                 }
+                br.readLine();
             }
+            boolean canEscape = false;
+            while(!queue.isEmpty()){
+                Tuple t = queue.poll();
+                if(t.z == tz && t.x == tx && t.y == ty) {
+                    canEscape = true;
+                    break;
+                }
+                for(int dir = 0; dir < 6; dir++){
+                    int nz = t.z + dz[dir]; //높이
+                    int nx = t.x + dx[dir]; //행
+                    int ny = t.y + dy[dir]; //열
 
-            bfs(start);
-            if(!check) {
-                System.out.println("Trapped!");
+                    if(nx < 0 || ny < 0 || nz < 0 || nz >= L || nx >= R || ny >= C) continue;
+                    if(MAP[nz][nx][ny] == '#') continue;
+                    if(dist[nz][nx][ny] != 0) continue;
+                    queue.offer(new Tuple(nz, nx, ny));
+                    dist[nz][nx][ny] = dist[t.z][t.x][t.y] + 1;
+                }
             }
+            if(canEscape) printCanEscape(dist[tz][tx][ty]-1);
+            else printCannotEscaped();
         }
 
-
-    }
-    public static void bfs(Po p) {
-        Queue<Po> queue = new LinkedList<>();
-        queue.add(p);
-        visit[p.z][p.y][p.x] = true;
-
-        while(!queue.isEmpty()) {
-            Po tmp = queue.poll();
-            if(tmp.x == end.x && tmp.y == end.y && tmp.z == end.z) {
-
-                System.out.println("Escaped in "+map[tmp.z][tmp.y][tmp.x]+" minute(s).");
-                check = true;
-                return;
-            }
-
-            for(int d=0; d<6; d++) {
-                int newX = tmp.x + moveX[d];
-                int newY = tmp.y + moveY[d];
-                int newZ = tmp.z + moveZ[d];
-
-                if(0<=newX && newX<C && 0<=newY && newY<R && 0<=newZ && newZ<L && !visit[newZ][newY][newX]) {
-                    if(arr[newZ][newY][newX]!='#') {
-                        visit[newZ][newY][newX] = true;
-                        map[newZ][newY][newX] = map[tmp.z][tmp.y][tmp.x] + 1;
-                        queue.add(new Po(newX,newY,newZ));
-                    }
-                }
-            }
+        for (String s : ret) {
+            System.out.println(s);
         }
 
     }
-    public static class Po{
-        int x;
-        int y;
-        int z;
-        public Po(int x,int y,int z) {
-            this.x=x;
-            this.y=y;
-            this.z=z;
-        }
+
+    static void printCanEscape(int x){
+        String str = "Escaped in " + x + " minute(s).";
+        ret.add(str);
+    }
+    static void printCannotEscaped(){
+        ret.add("Trapped!");
     }
 }
