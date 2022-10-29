047bd84801dac9f46a6c7cd06c443dc4ce069579
hoin <apicad0307@gmail.com>
2022-10-29 14:55:22 +0900

    비숍

--- Algorithm/day19/P3.java
+++ Algorithm/day19/P3.java
@@ -1,70 +1,67 @@
 package Algorithm.day19;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.util.StringTokenizer;
 
 public class P3 {
-
     static int N;
     static int[][] board;
-    static boolean[][] vis;
-    static int ans = 0;
+    static boolean[][] visited, isBlack;
+    static int[] res;
     static int[] dx = {-1, -1, 1, 1};
-    static int[] dy = {-1, 1, 1, -1};
+    static int[] dy = {-1, 1, -1, 1};
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         board = new int[N][N];
-        vis = new boolean[N][N];
+        visited = new boolean[N][N];
+        isBlack = new boolean[N][N];
+        res = new int[2];
         for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
+            for (int j = 0; j < N; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
+                isBlack[i][j] = (i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1);
             }
         }
-        chess(board, 0);
-        System.out.println(ans);
+
+        dfs(0, true, 0);
+        dfs(0, false, 0);
+        System.out.println(res[0] + res[1]);
     }
 
-    private static void chess(int[][] myBoard, int count) {
-        int[][] tmp = new int[N][N];
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                tmp[i][j] = myBoard[i][j];
-            }
-        }
-//        int[][] tmp = myBoard.clone();
+    static void dfs(int idx, boolean black, int cnt) {
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if (tmp[i][j] == 1 && vis[i][j] == false) {
-                    vis[i][j] = true;
-                    put(i, j, tmp);
-                    chess(tmp, count + 1);
-                    tmp = myBoard.clone();
-                }
-            }
+        for(int i=idx; i<N*N; i++){
+            int x = i / N;
+            int y = i % N;
+
+            if(board[x][y] == 0 || isBlack[x][y] != black || !check(x, y)) continue;
+            visited[x][y] = true;
+            dfs(i + 1, black, cnt + 1);
+            visited[x][y] = false;
         }
-//        System.out.println(board);
-        ans = Math.max(ans, count);
+        res[black ? 0 : 1] = Math.max(res[black ? 0 : 1], cnt);
     }
 
-    private static void put(int i, int j, int[][] board) {
-        board[i][j] = 0;
-        for(int dir=0; dir<4; dir++){
-            int cnt = 1;
+    static boolean check(int x, int y) {
+        for (int dir = 0; dir < 4; dir++) {
+            int cnt = 0;
             while (true) {
-                int nx = i + dx[dir] * cnt;
-                int ny = j + dy[dir] * cnt;
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N ) break;
-                board[nx][ny] = 0;
+                int nx = x + (dx[dir] * cnt);
+                int ny = y + (dy[dir] * cnt);
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N) break;
+                if(visited[nx][ny]) return false;
                 cnt++;
             }
         }
+        return true;
     }
 
 }
