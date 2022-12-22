a16197bb60be1b8bbeb19a8c98a7439f8e8e66cb
hoin <apicad0307@gmail.com>
2022-12-22 22:52:53 +0900

    어레이

---
+++ Algorithm/day32/p6.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int m, n;
+int arr[102][10002];
+int main(){
+    cin>>m>>n;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"arr";
+}
