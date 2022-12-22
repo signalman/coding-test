322e7b619d59ce374fb875a10a8d55b0db6b93fc
hoin <apicad0307@gmail.com>
2022-12-23 00:40:55 +0900

    feat: 멀티버스

--- Algorithm/day32/p6.cpp
+++ Algorithm/day32/p6.cpp
@@ -1,14 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int m, n;
-int arr[102][10002];
+int m, n; //m 俺狼 快林, n俺狼 青己
+int arr[101][10001];
+
+void compress(int arr[]){
+    vector<int> v(arr, arr+n);
+
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    for(int i=0; i<n; i++){
+        arr[i] = lower_bound(v.begin(), v.end(), arr[i]) - v.end();
+    }
+}
+
+bool compare(int a[], int b[]){
+    for(int i=0; i<n; i++){
+        if(a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     cin>>m>>n;
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
             cin>>arr[i][j];
         }
     }
-    cout<<"arr";
+
+    for(int i=0; i<m; i++){
+        compress(arr[i]);
+    }
+    int ans = 0;
+
+    for(int i=0; i<m-1; i++){
+        for(int j=i+1; j<m; j++){
+            ans += compare(arr[i], arr[j]);
+        }
+    }
+    cout<<ans;
 }
