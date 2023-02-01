4c662bd602489674a5e268c0158131c65d46c299
Hoin Shin <apicad0307@gmail.com>
2023-02-01 23:07:37 +0900

    [Silver III] Title: 수강신청, Time: 552 ms, Memory: 25624 KB -BaekjoonHub

---
+++ 백준/Silver/13414. 수강신청/수강신청.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int k, l;
+map<string, int> m;
+bool cmp(pair<string, int>& a, pair<string, int>& b) {
+	return a.second < b.second;
+}
+
+int main() {
+	cin >> k >> l;
+	for (int i = 1; i <= l; i++) {
+		string str;
+		cin >> str;
+		m[str] = i;
+	}
+
+	vector<pair<string, int>> v(m.begin(), m.end());
+	//sort(v.begin(), v.end(), [](auto& a, auto& b) {return a.second < b.second; });
+	sort(v.begin(), v.end(), cmp);
+	k = min(k, (int)v.size());
+	for (int i = 0; i < k; i++) {
+		cout << v[i].first << '\n';
+	}
+
+
+}
