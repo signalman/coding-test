2c2d3517519e297e42157f93191a94d95c318da0
Hoin Shin <apicad0307@gmail.com>
2023-02-13 19:41:25 +0900

    [Gold V] Title: 탑, Time: 652 ms, Memory: 110920 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Pair{
+        int x, y;
+        Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static int[] arr;
+    static int[] ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new int[N+1];
+        ans = new int[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Pair> stack = new Stack<>();
+        for(int i=N; i>=1; i--){
+            while(!stack.isEmpty() && stack.peek().x < arr[i]){
+                ans[stack.peek().y] = i;
+                stack.pop();
+            }
+            stack.add(new Pair(arr[i], i)); //값, 위치
+        }
+        while (!stack.isEmpty()) {
+            ans[stack.pop().y] = 0;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            sb.append(ans[i]).append(" ");
+        }
+        System.out.println(sb);
+
+    }
+}
