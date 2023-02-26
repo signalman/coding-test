c01bf9539e1bd0b6c2cef126ca7bd5f5f2be4bd0
hoin <apicad0307@gmail.com>
2023-02-26 22:12:51 +0900

    feat: 감시

---
+++ Algorithm/day35/감시.java
@@ -0,0 +1,123 @@
+package Algorithm.day35;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class 감시 {
+
+    static int N, M;
+    static char[][] map;
+    static ArrayList<int[]> cctv; //cctv 좌표 저장
+    //하, 좌, 상, 우
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static int answer = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
+
+        cctv = new ArrayList<>();
+
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++) {
+                map[i][j] = st.nextToken().charAt(0);
+                if(map[i][j] != '0' && map[i][j] != '6') cctv.add(new int[] {i, j});
+            }
+        }
+
+        //모든 경우의수 구하기
+        for(int brute = 0; brute < (1<<2*cctv.size()); brute++) {
+
+            int tmp = brute;
+            //cctv별 방향 저장
+            int[] quarter = new int[cctv.size()];
+
+            for(int i=cctv.size()-1; i>=0; i--) {
+                int d = tmp % 4;
+                quarter[i] = d;
+                tmp /= 4;
+            }
+            solve(quarter);
+        }
+        System.out.println(answer);
+    }
+
+    private static void solve(int[] quarter) {
+
+        //이번에 사용할 맵
+        char[][] _map = new char[N][M];
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                _map[i][j] = map[i][j];
+            }
+        }
+
+        //각 cctv별 감시 위치 파악
+        for(int i=0; i<cctv.size(); i++) {
+
+            int dir = quarter[i];
+            int x = cctv.get(i)[0];
+            int y = cctv.get(i)[1];
+
+            switch(_map[x][y]) {
+                case '1':
+                    watch1(x, y, dir, _map);
+                    break;
+                case '2':
+                    watch1(x, y, dir, _map);
+                    watch1(x, y, (dir + 2) % 4, _map);
+                    break;
+
+                case '3':
+                    watch1(x, y, dir, _map);
+                    watch1(x, y, (dir + 1) % 4, _map);
+                    break;
+
+                case '4':
+                    watch1(x, y, dir, _map);
+                    watch1(x, y, (dir + 1) % 4, _map);
+                    watch1(x, y, (dir + 3) % 4, _map);
+                    break;
+
+                case '5':
+                    watch1(x, y, dir, _map);
+                    watch1(x, y, (dir + 1) % 4, _map);
+                    watch1(x, y, (dir + 2) % 4, _map);
+                    watch1(x, y, (dir + 3) % 4, _map);
+                    break;
+            }
+        }
+
+        //사각지대 개수
+        int cnt = 0;
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(_map[i][j] == '0') cnt++;
+            }
+        }
+        answer = Math.min(answer, cnt);
+    }
+
+    static void watch1(int x, int y, int dir, char[][] _map) {
+
+        while(true) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if(nx < 0 || ny < 0 || nx >= N || ny >= M) break;
+            if(_map[nx][ny] == '6') break;
+
+            _map[nx][ny] = '7';
+            x = nx;
+            y = ny;
+        }
+    }
+}
---
+++ Algorithm/day35/알파벳.java
@@ -0,0 +1,73 @@
+package Algorithm.day35;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class 알파벳 {
+
+    static int R, C;
+    static char[][] map;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int answer = 1;
+    static ArrayList<Character> list;
+
+    static class Token{
+        int x, y;
+        List<Character> record;
+        Token(int x, int y, List record){
+            this.x = x;
+            this.y = y;
+            this.record = record;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char[R][C];
+
+        for(int i=0; i<R; i++){
+            map[i] = br.readLine().toCharArray();
+        }
+        list = new ArrayList<>();
+        list.add(map[0][0]);
+        solve(0, 0);
+        System.out.println(answer);
+    }
+
+    private static void solve(int x, int y) {
+        Queue<Token> queue = new ArrayDeque<>();
+        Token token = new Token(x, y, new ArrayList<>(Arrays.asList(map[x][y])));
+        queue.add(token);
+
+        while (!queue.isEmpty()) {
+            Token cur = queue.poll();
+
+            for(int dir =0; dir <4; dir++){
+                int nx = cur.x + dx[dir];
+                int ny = cur.y + dy[dir];
+                //Deep copy 해야함
+                List<Character> record = new ArrayList<>();
+                for (Character c : cur.record) {
+                    record.add(c);
+                }
+                if(nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+                if(isPossible(cur, nx, ny)){
+                    record.add(map[nx][ny]);
+                    queue.add(new Token(nx, ny, record));
+                    answer = Math.max(answer, record.size());
+                }
+            }
+        }
+    }
+
+    private static boolean isPossible(Token cur, int nx, int ny) {
+        return !cur.record.contains(map[nx][ny]);
+    }
+}
