ca393448f7a8b0e84c0b38cc80931c3fb8c5f2ab
hoin <apicad0307@gmail.com>
2023-03-05 21:01:11 +0900

    feat: 최단경로2

--- Algorithm/day37/p5.cpp
+++ Algorithm/day37/p5.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = 1e5+5;
 int n, m;
-vector<pair<int, int>> adj[MX];
+vector<pair<int, int>> adj[1005];
 int d[1005];
 int p[1005];
 int main(){
@@ -20,6 +19,7 @@ int main(){
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     d[st] = 0;
     pq.push({d[st], st});
+    //p[st] = st;
     while(!pq.empty()){
         auto cur = pq.top(); pq.pop();
 
@@ -27,17 +27,20 @@ int main(){
         for(auto nxt: adj[cur.second]){
             if(d[nxt.second] <= d[cur.second] + nxt.first) continue;
             d[nxt.second] = d[cur.second] + nxt.first;
-            p[nxt.second] = p[cur.second];
+            p[nxt.second] = cur.second;
             pq.push({d[nxt.second], nxt.second});
         }
     }
     cout<<d[en]<<'\n';
-
-    while(true){
-        cout<<p[en];
-        if(p[en] == st) break;
-        en = p[en];
-
+    vector<int> path;
+    int cur = en;
+    while(cur != st){
+        path.push_back(cur);
+        cur = p[cur];
     }
+    path.push_back(cur);
+    reverse(path.begin(), path.end());
+    cout<<path.size()<<'\n';
+    for(auto x: path) cout<< x <<' ';
 
 }
