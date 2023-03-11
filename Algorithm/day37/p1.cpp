b32b9701c4ac523a102291cd7ee0e50c042a8ba8
hoin <apicad0307@naver.com>
2023-03-12 02:53:56 +0900

    feat: 배추심기

--- Algorithm/day37/p1.cpp
+++ Algorithm/day37/p1.cpp
@@ -1,37 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     string board;
     string ans;
-    cin>>board;
+    cin >> board;
 
     int cnt = 0;
     board += " ";
-    for(int i=0; i<board.size()-1; i++){
+    for (int i = 0; i < board.size() - 1; i++)
+    {
 
-        if(board[i] == '.'){
-            if(cnt % 2 != 0) break;
+        if (board[i] == '.')
+        {
+            if (cnt % 2 != 0)
+                break;
             ans += ".";
             cnt = 0;
         }
 
-        if(board[i] == 'X'){
+        if (board[i] == 'X')
+        {
             cnt++;
         }
-        //X가 4개일때
-        if(cnt == 4){
+        // X가 4개일때
+        if (cnt == 4)
+        {
             ans += "AAAA";
             cnt = 0;
         }
-        //X가 2개이고 다음번이 X가 아닐 때
-        if(cnt == 2 && board[i+1] != 'X'){
+        // X가 2개이고 다음번이 X가 아닐 때
+        if (cnt == 2 && board[i + 1] != 'X')
+        {
             ans += "BB";
             cnt = 0;
         }
     }
-    if(cnt % 2 != 0) cout<< -1;
-    else cout<<ans;
-
-
+    if (cnt % 2 != 0)
+        cout << -1;
+    else
+        cout << ans;
 }
