12a24808a2a9ec47394155cde458f0111335d818
hoin <apicad0307@naver.com>
2023-03-14 23:39:29 +0900

    feat: 코딩 테스트 공부

--- Algorithm/day38/p4.cpp
+++ Algorithm/day38/p4.cpp
@@ -1,38 +1,61 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
 using namespace std;
 
-int N;
-const int MX = 1e9;
-pair<long long, int> d[101][10];
+long long dp[1 << 10][101][10];
+
 int main()
 {
-    cin >> N;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if (n <= 9)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+
     for (int i = 1; i <= 9; i++)
     {
-        d[1][i].first = 1;
-        d[1][i].second = (1 << i);
+        dp[1 << i][1][i] = 1;
     }
-
-    for (int i = 2; i <= N; i++)
-    {
-        for (int j = 0; j <= 9; j++)
-        {
-
-            if (j == 0)
-                d[i][j].first = d[i - 1][1].first % MX;
-            d[i][j].second = (d[i - 1][1].second | (1 << j));
-            else if (j == 9)
-                d[i][j]
-                    .first = d[i - 1][8].first % MX;
-            d[i][j].second = (d[i - 1][8].second | (1 << j));
-            else d[i][j].first = (d[i - 1][j - 1].first + d[i - 1][j + 1].first) % MX;
+    //(int)10e8;
+    for (int i = 2; i <= n; i++)
+    { // i는 숫자 갯수
+        for (int k = 0; k <= 9; k++)
+        { // k는 마지막 숫자 번호
+            for (int bit = 0; bit < (1 << 10); bit++)
+            { // bit는 이전 비트
+                if (k == 0)
+                {
+                    dp[bit | (1 << k)][i][k] += dp[bit][i - 1][k + 1] % (int)10e8;
+                }
+                else if (k == 9)
+                {
+                    dp[bit | (1 << k)][i][k] += dp[bit][i - 1][k - 1] % (int)10e8;
+                }
+                else
+                {
+                    dp[bit | (1 << k)][i][k] += (dp[bit][i - 1][k + 1] + dp[bit][i - 1][k - 1]) % (int)10e8;
+                }
+            }
         }
     }
-    long long ans = 0;
-    // for (int i = 0; i <= 9; i++)
-    // {
-    //     cout << d[N][i].second << ' ';
-    // }
-    // cout << ans % MX;
-    cout << d[2][1].second << ' ' << d[2][4].second;
+
+    long long answer = 0;
+
+    for (int i = 0; i <= 9; i++)
+    {
+        answer = (answer + dp[(1 << 10) - 1][n][i]) % (int)10e8;
+    }
+
+    cout << answer << endl;
+
+    return 0;
 }
---
+++ Algorithm/day38/p5.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int alp, int cop, vector<vector<int>> problems)
+{
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+}
