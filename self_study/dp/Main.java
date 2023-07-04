6b21c0b9be2abeccf124b3fdf16faab80f75ceeb
hoin <apicad0307@naver.com>
2023-07-04 22:06:51 +0900

    feat: 우유 도시

--- self_study/dp/Main.java
+++ self_study/dp/Main.java
@@ -1,62 +1,72 @@
-package self_study.dp;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-//딸, 초, 바, 딸
-
-//dp[i][j][k][l] : i,j의 음식점에서 먹고/안먹고  마지막먹은 아이스크림[l]
 public class Main {
-    static int dy[] = { 1, 0 };
-    static int dx[] = { 0, 1 };
-    static int[][] V;
-    static int[][][] DP;
-    static int N;
-    static int solve(int x, int y, int milk){
-        int ret = DP[x][y][milk + 1];
-        if(ret != -1) return ret;
-        ret = 0;
+    static int n;				// n x n 행렬
+    static int[][] map;
+    static int[][][] dp;
+    static int maxCount;		// 출력, 마시는 우유 최대 개수
 
-        for(int dir = 0; dir < 2; dir++){
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-            if (V[nx][ny] == (milk + 1) % 3) {
-                ret = Math.max(ret, solve(nx, ny, (milk + 1) % 3) + 1);
-            }
-            ret = Math.max(ret, solve(nx, ny, milk));
-        }
-//        DP[x][y][milk] = ret;
-        return ret;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+    static void solution() {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int currentMilk = map[i][j];	// 현재 위치의 우유
 
-        V = new int[N][N];
-        DP = new int[N][N][4];
+                // 우유 마시는 순서: 딸기(0) -> 초코(1) -> 바나나(2)
+                if (currentMilk == 0) {
+                    // 이전 순서의 바나나(2)를 최근으로 마신 개수 + 1
+                    dp[i][j][0] = Math.max(dp[i][j-1][2] + 1, dp[i-1][j][2] + 1);
+                }
+                else {
+                    dp[i][j][0] = Math.max(dp[i][j-1][0], dp[i-1][j][0]);
+                }
 
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                V[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+                // 추가 조건식: 우유 마시는 순서 지키기 위함 (해당 현재 우유 currentMilk 를 마셔도 되는지 확인)
+                if (currentMilk == 1 &&
+                        dp[i][j][0] > dp[i][j][1]) {	// 이전에 딸기(0)를 먹었는지
+                    // 이전 순서의 딸기(0)를 최근으로 마신 개수 + 1
+                    dp[i][j][1] = Math.max(dp[i][j-1][0] + 1, dp[i-1][j][0] + 1);
+                }
+                else {
+                    dp[i][j][1] = Math.max(dp[i][j-1][1], dp[i-1][j][1]);
+                }
 
-        init();
-        int ans = V[0][0] == 0 ? solve(0, 0, 0) + 1 : solve(0, 0, -1);
-        System.out.println(ans);
-    }
-
-    private static void init() {
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                for(int k=0; k<4; k++){
-                    DP[i][j][k] = -1;
+                if (currentMilk == 2 &&
+                        dp[i][j][1] > dp[i][j][2]) {	// 이전에 초코(1)를 먹었는지
+                    // 이전 순서의 초코(1)를 최근으로 마신 개수 + 1
+                    dp[i][j][2] = Math.max(dp[i][j-1][1] + 1, dp[i-1][j][1] + 1);
+                }
+                else {
+                    dp[i][j][2] = Math.max(dp[i][j-1][2], dp[i-1][j][2]);
                 }
             }
         }
+
+        // maxCount 찾기
+        maxCount = Math.max(
+                dp[n][n][0],
+                Math.max(dp[n][n][1], dp[n][n][2])
+        );
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in)
+        );
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n + 1][n + 1];		// [1][1] ~ [n][n] 사용
+        dp = new int[n + 1][n + 1][3];		// [0]행, [0]열은 패딩
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= n; j++)
+                map[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        solution();
+        System.out.println(maxCount);
     }
 }
