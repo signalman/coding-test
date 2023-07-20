624e31b2be8208f3196f1f9086897735a8b1c2ef
hoin <apicad0307@naver.com>
2023-07-20 23:55:44 +0900

    생성자

---
+++ Algorithm/day42/Main.java
@@ -0,0 +1,41 @@
+package Algorithm.day42;
+
+class Parent {
+    int x = 100;
+
+    Parent() {
+        this(500);
+    }
+
+    Parent(int x) {
+        this.x = x;
+    }
+
+    int getX() {
+        return x;
+    }
+}
+
+class Child extends Parent {
+    int x = 4000;
+
+    Child() {
+        this(5000);
+    }
+
+    Child(int x) {
+        this.x = x;
+    }
+
+    @Override
+    int getX() {
+        return x;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Child obj = new Child();
+        System.out.println(obj.getX());
+    }
+}
