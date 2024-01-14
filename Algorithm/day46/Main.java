6889996d7219f9fc08a034bb00b39812936e8341
hoin <apicad0307@naver.com>
2024-01-15 02:38:20 +0900

    Bronze

---
+++ Algorithm/day46/Main.java
@@ -0,0 +1,16 @@
+package Algorithm.day46;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        char c = s.charAt(i-1);
+        System.out.println(c);
+    }
+}
