710d524d312a1fcbb020a419f9a722238fe5f053
hoin <apicad0307@naver.com>
2024-02-08 15:36:07 +0900

    max as Int

---
+++ Algorithm/day49/Solve.java
@@ -0,0 +1,26 @@
+package Algorithm.day49;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Solve {
+
+    public static void main(String[] args) {
+        double a = 1.12345;
+        double v = a - (int) a;
+
+        long l = Long.MAX_VALUE;
+        long max = Math.max(l, 12312);
+        List<Integer> list = new ArrayList<>();
+        int frequency = Collections.frequency(list, 23);
+        System.out.println(frequency);
+        int asInt = Arrays.stream(new int[]{1, 2, 2, 13})
+                          .max()
+                          .getAsInt();
+
+
+        System.out.println(v);
+    }
+}
