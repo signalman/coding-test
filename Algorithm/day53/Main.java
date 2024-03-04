5d86863b02306e847baf20d7db292c27af02a87a
hoin <apicad0307@naver.com>
2024-03-04 15:28:47 +0900

    아이템줍기

--- Algorithm/day53/Main.java
+++ Algorithm/day53/Main.java
@@ -3,60 +3,58 @@ package Algorithm.day53;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-public class Main {
-    int[][] map;
+class Main {
     int[] dx = new int[]{1, 0, -1, 0};
     int[] dy = new int[]{0, 1, 0, -1};
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        int answer = 0;
-        map = new int[51][51];
 
-        //모든 직사각형 색칠
+        int[][] map = new int[101][101];
+        characterX *= 2;
+        characterY *= 2;
+        itemX *= 2;
+        itemY *= 2;
+
         for(int[] r: rectangle){
-            int x1 = r[0];
-            int y1 = r[1];
-            int x2 = r[2];
-            int y2 = r[3];
 
-            for(int i = x1; i <= x2; i++){
-                for(int j = y1; j <= y2; j++){
-                    map[i][j] = 1;
+            r[0] *= 2;
+            r[1] *= 2;
+            r[2] *= 2;
+            r[3] *= 2;
+
+            for(int x = r[0]; x <= r[2]; x++){
+                for(int y = r[1]; y <= r[3]; y++){
+                    //라인일때
+                    if(x == r[0] || x == r[2] || y == r[1] || y == r[3]) {
+                        if(map[x][y] == -1) continue;
+                        map[x][y] = 1;
+                    }
+                    else map[x][y] = -1; //내부 표시
                 }
             }
         }
-        //라인트레이싱 bfs
 
         Queue<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[51][51];
-        queue.add(new int[]{characterX, characterY, 0});
+        boolean[][] visited = new boolean[101][101];
+
+        queue.add(new int[]{characterX, characterY, 0}); //좌표 이동거리
         visited[characterX][characterY] = true;
 
         while(!queue.isEmpty()){
 
             int[] cur = queue.poll();
-
-            if(isInside(cur, map)) continue;
-            if(cur[0] == itemX && cur[1] == itemY) return cur[2];
-            System.out.println(cur[0] + " "+ cur[1]);
+            if(cur[0] == itemX && cur[1] == itemY) return cur[2] / 2;
 
             for(int dir = 0; dir < 4; dir++){
+
                 int nx = cur[0] + dx[dir];
                 int ny = cur[1] + dy[dir];
 
-                if(nx < 0 || ny < 0 || nx >= 51 || ny >= 51) continue;
-                if(visited[nx][ny] || map[nx][ny] == 0) continue;
+                if(nx < 0 || ny < 0 || nx >= 101 || ny >= 101) continue;
+                if(visited[nx][ny] || map[nx][ny] != 1) continue;
+                queue.add(new int[]{nx, ny, cur[2] + 1});
                 visited[nx][ny] = true;
-                queue.add(new int[]{nx, ny, cur[0] + 1});
             }
         }
         return -1;
-    }
-    boolean isInside(int[] cur, int[][] map){
-        for(int dir = 0; dir < 4; dir++){
-            int nx = cur[0] + dx[dir];
-            int ny = cur[1] + dy[dir];
-            if(map[nx][ny] == 0) return false;
-        }
-        return true;
     }
 }
