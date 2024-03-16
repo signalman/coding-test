36249c044da3d6ca4c88efb9e2a8d97aafb14839
hoin <apicad0307@naver.com>
2024-03-16 14:10:21 +0900

    join 활용하기

---
+++ Algorithm/day55/Main2.java
@@ -0,0 +1,9 @@
+package Algorithm.day55;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        String join = String.join("-", "kevin", "is", "cool");
+        System.out.println(join);
+    }
+}
