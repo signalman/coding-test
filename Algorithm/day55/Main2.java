17b7d61b9e495dcf6754cfd2dff62689e8d65e0a
hoin <apicad0307@naver.com>
2024-03-18 05:45:14 +0900

    순위검색

--- Algorithm/day55/Main2.java
+++ Algorithm/day55/Main2.java
@@ -1,114 +1,87 @@
 package Algorithm.day55;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main2 {
+    public int[] solution(String[] info, String[] query) {
+        List<Integer> answer = new ArrayList<>();
+        Map<String, List<Integer>> db = new HashMap<>();
+        init(db);
 
-    //문자열을 숫자로 매핑시키기
-// cpp 0, java 1, python 2
-// backend 0, frontend 1
-// junior 0, senior 1
-// chicken 0, pizza 1
-//score 1 ~ 100000
-    class Solution {
-
-        Map<String, Integer> hash = Map.of("cpp", 0, "java", 1, "python", 2, "backend", 0, "frontend", 1, "junior", 0, "senior", 1, "chicken", 0, "pizza", 1);
-
-        public int[] solution(String[] info, String[] query) {
-            List<Integer> answer = new ArrayList<>();
-            int[][] infos = new int[info.length][2]; //문자열 점수, 점수
-
-            //infos에 넣기
-            for(int i=0; i<info.length; i++){
-                String[] sp = info[i].split(" ");
-                infos[i] = toNumber(sp, hash);
-            }
-            //정렬하기
-            Arrays.sort(infos, Comparator.comparing((int[] o) -> o[0]).thenComparing(o -> o[1]));
-            System.out.println(Arrays.deepToString(infos));
-            System.out.println("+++++++++");
-            for(String q: query){
-                q = q.replace(" and ", " ");
-                int[] range = queryToRange(q); //01010, 01111, 100점
-                System.out.println(Arrays.toString(range));
-                //binarySearch 수행
-                int lo = lowerBound(infos, range);
-                //binarySearch 수행
-                int hi  = upperBound(infos, range);
-                // System.out.println(hi + " " + lo);
-                answer.add(hi - lo);
-            }
-
-            return answer.stream().mapToInt(i -> i).toArray();
+        for (String s : info) {
+            String[] split = s.split(" ");
+            pushDb(split, db);
+        }
+        for (String s : db.keySet()) {
+            db.get(s).sort(Comparator.naturalOrder());
         }
 
-        int lowerBound(int[][] array, int[] range){
-
-            int st = 0;
-            int en = array.length - 1;
-
-            while(st < en){
-
-                int mid = (st + en) / 2;
-                //조건 만족
-                if(array[mid][0] >= range[0] && array[mid][1] >= range[2]){
-                    en = mid;
-                }
-                else{
-                    st = mid + 1;
-                }
-            }
-            return st;
+        for (String s : query) {
+            String[] split = s.replace("and ", "").split(" ");
+            int count = findMembers(split, db);
+            answer.add(count);
         }
 
-        int upperBound(int[][] array, int[] range){
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
 
-            int st = 0;
-            int en = array.length - 1;
+    private int findMembers(String[] split, Map<String, List<Integer>> db) {
+        String query = split[0] + split[1] + split[2] + split[3];
+        int score = Integer.parseInt(split[4]);
 
-            while(st < en){
+        List<Integer> list = db.get(query);
+        if(list.isEmpty()) return 0;
 
-                int mid = (st + en ) / 2;
-                //조건 만족
-                if(array[mid][0] > range[1]){
-                    en = mid;
-                }
-                else{
-                    st = mid + 1;
-                }
-            }
-            return st;
+        int st = 0;
+        int en = list.size();
+
+        while(st < en){
+            int mid = (st + en) / 2;
+
+            if(list.get(mid) >= score) en = mid;
+            else st = mid + 1;
         }
+        return list.size() - st;
+    }
 
-        int[] toNumber(String[] split, Map<String, Integer> hash){
-
-            String binary = "";
-            for(int i=0; i<split.length -1; i++){
-                binary += hash.get(split[i]).toString();
+    private void pushDb(String[] split, Map<String, List<Integer>> db) {
+        List<List<String>> unit = new ArrayList<>();
+        for (int i = 0; i < split.length - 1; i++) {
+            if (split[i].equals("-")) {
+                unit.add(List.of("-"));
+            } else {
+                unit.add(List.of("-", split[i]));
             }
-            return new int[]{Integer.parseInt(binary, 3), Integer.parseInt(split[split.length - 1])};
         }
-        //해당 쿼리가 범위를 나타내도록 만드는 함수
-        int[] queryToRange(String query){
-            String[] split = query.split(" ");
-            String mn = "";
-            String mx = "";
+        dfs("", 0, unit, db, Integer.parseInt(split[split.length - 1]));
+    }
 
-            //java/ backend/ - /pizza/ 110
-            for(int i=0; i<split.length - 1; i++){
-                if(split[i].equals("-")){
-                    mn += "0";
-                    mx += "1";
-                } else{
-                    mn += hash.get(split[i]).toString();
-                    mx += hash.get(split[i]).toString();
-                }
-            }
-            return new int[]{Integer.parseInt(mn, 3), Integer.parseInt(mx, 3), Integer.parseInt(split[split.length - 1])};
+    private void dfs(String tmp, int index, List<List<String>> unit, Map<String, List<Integer>> db, int score) {
+        if(index == 4){
+            db.get(tmp).add(score);
+            return;
+        }
+        for(int i=0; i<unit.get(index).size(); i++){
+            dfs(tmp + unit.get(index).get(i), index + 1, unit, db, score);
+        }
+    }
+
+    private void init(Map<String, List<Integer>> db) {
+        String[][] unit = new String[][]{{"cpp", "java", "python", "-"}, {"backend", "frontend", "-"}, {"junior", "senior", "-"}, {"chicken", "pizza", "-"}};
+        dfs("", 0, unit, db);
+    }
+
+    private void dfs(String tmp, int index, String[][] unit, Map<String, List<Integer>> db) {
+        if(index == 4) {
+            db.put(tmp, new ArrayList<>());
+            return;
+        }
+        for(int i=0; i<unit[index].length; i++){
+            dfs(tmp + unit[index][i], index + 1, unit, db);
         }
     }
 }
